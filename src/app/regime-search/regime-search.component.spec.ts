78f42f42e0f800425b9b5b64c9ac085e1864afe1
Frozzie89 <froztor85@gmail.com>
2024-10-10 12:17:05 +0200

    fixed test configs

--- src/app/regime-search/regime-search.component.spec.ts
+++ src/app/regime-search/regime-search.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { RegimeSearchComponent } from './regime-search.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('RegimeSearchComponent', () => {
     let component: RegimeSearchComponent;
@@ -9,7 +10,8 @@ describe('RegimeSearchComponent', () => {
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
-            imports: [IonicModule.forRoot(), RegimeSearchComponent]
+            imports: [IonicModule.forRoot(), RegimeSearchComponent],
+            providers: [provideHttpClient()]
         }).compileComponents();
 
         fixture = TestBed.createComponent(RegimeSearchComponent);
--- src/app/service/regime-data.service.spec.ts
+++ src/app/service/regime-data.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RegimeDataService } from './regime-data.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('RegimeDataService', () => {
     let service: RegimeDataService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [provideHttpClient()]
+        });
         service = TestBed.inject(RegimeDataService);
     });
 
