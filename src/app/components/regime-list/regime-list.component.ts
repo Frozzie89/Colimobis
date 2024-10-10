2266a12b8edc4d8e527e0c23ed2fd393955f21b5
Frozzie89 <froztor85@gmail.com>
2024-10-10 13:35:39 +0200

    regime-list display data in table

--- src/app/components/regime-list/regime-list.component.ts
+++ src/app/components/regime-list/regime-list.component.ts
@@ -1,15 +1,28 @@
+import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Regime } from 'src/app/classes/regime';
+import { RegimeDataService } from 'src/app/service/regime-data.service';
 
 @Component({
     standalone: true,
     selector: 'app-regime-list',
     templateUrl: './regime-list.component.html',
     styleUrls: ['./regime-list.component.scss'],
+    imports: [NgFor]
 })
 export class RegimeListComponent implements OnInit {
 
-    constructor() { }
+    regimeList: Regime[] = []
 
-    ngOnInit() { }
+    constructor(
+        private route: ActivatedRoute,
+        private regimeService: RegimeDataService
+    ) { }
+
+    ngOnInit() {
+        const search = this.route.snapshot.params['search']
+        this.regimeList = this.regimeService.regimeList.filter(regime => regime.id.includes(search))
+    }
 
 }
