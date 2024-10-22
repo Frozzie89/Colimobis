db13c9e159891b8b34f462e5d928070bfac8296a
Frozzie89 <froztor85@gmail.com>
2024-10-22 21:59:12 +0200

    new  'state' field in Regime class

--- src/app/classes/regime.ts
+++ src/app/classes/regime.ts
@@ -4,17 +4,24 @@ export class Regime {
     ot: number
     rf: string
     label: string
+    state: regimeState
 
-    constructor(id: string = '', requestNumber: string = '', ot: number = 0, rf: string = '', label: string = '') {
+    constructor(id: string = '', requestNumber: string = '', ot: number = 0, rf: string = '', label: string = '', state: regimeState = regimeState.RENDU_NON_TERMINE) {
         this.id = id
         this.requestNumber = requestNumber
         this.ot = ot
         this.rf = rf
         this.label = label
+        this.state = state
     }
 
     toString(): string {
         return `[Id = ${this.id}, RequestNumber = ${this.requestNumber}, Ot = ${this.ot}, Rf = ${this.rf}, Label = ${this.label}]`
     }
 
+}
+
+
+export enum regimeState {
+    AUTORISE, ERREUR, RENDU_NON_TERMINE
 }
