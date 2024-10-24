5db24dd9addf3e5a077ddbce418ffa6c265301be
Frozzie89 <froztor85@gmail.com>
2024-10-24 21:35:25 +0200

    new regime-restore component

--- src/app/components/action/regime-restore/regime-restore.component.ts
+++ src/app/components/action/regime-restore/regime-restore.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+import { RegimeStatusComponent } from "../../modules/regime-status/regime-status.component";
+import { ActivatedRoute } from '@angular/router';
+import { RegimeDataService } from 'src/app/service/regime-data.service';
+import { Regime } from 'src/app/classes/regime';
 
 @Component({
     selector: 'app-regime-restore',
     templateUrl: './regime-restore.component.html',
     styleUrls: ['./regime-restore.component.scss'],
+    imports: [IonContent, RegimeStatusComponent],
     standalone: true
 })
 export class RegimeRestoreComponent implements OnInit {
 
-    constructor() { }
+    regime!: Regime
 
-    ngOnInit() { }
+    constructor(
+        private route: ActivatedRoute,
+        private regimeService: RegimeDataService
+    ) { }
+
+    ngOnInit() {
+        const id = this.route.snapshot.params['id']
+        this.regime = this.regimeService.regimeList.find(regime => regime.id.includes(id)) || new Regime()
+    }
+
 
 }
