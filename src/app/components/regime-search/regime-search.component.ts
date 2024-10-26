97a5cadb8e099cd2f8df780ed23f3fb64f961592
Frozzie89 <froztor85@gmail.com>
2024-10-26 13:05:53 +0200

    implemented new header module component in existing pages

--- src/app/components/regime-search/regime-search.component.ts
+++ src/app/components/regime-search/regime-search.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { HeaderComponent } from "../modules/header/header.component";
 
 @Component({
     standalone: true,
     selector: 'app-regime-search',
     templateUrl: './regime-search.component.html',
-    imports: [FormsModule, IonContent, IonGrid, IonRow, IonCol],
+    imports: [FormsModule, IonContent, IonGrid, IonRow, IonCol, HeaderComponent],
     styleUrls: ['./regime-search.component.scss'],
 })
 export class RegimeSearchComponent implements OnInit {
+    title = 'Sélection d\'attestations'
 
     constructor(
         private router: Router
