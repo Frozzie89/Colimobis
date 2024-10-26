39827ca2e33eb740b25774dae20bfc6d28c7da1e
Frozzie89 <froztor85@gmail.com>
2024-10-26 12:54:42 +0200

    new header module component

---
+++ src/app/components/modules/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+    imports: [IonHeader, IonToolbar, IonTitle],
+    standalone: true
+})
+export class HeaderComponent implements OnInit {
+
+    @Input() title = ''
+
+    constructor() { }
+
+    ngOnInit() { }
+
+}
