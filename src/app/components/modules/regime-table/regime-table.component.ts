d91ae1e0e94eec2eed825934c32e727e808a3583
Frozzie89 <froztor85@gmail.com>
2024-11-01 13:54:06 +0100

    new actions col in regime table

--- src/app/components/modules/regime-table/regime-table.component.ts
+++ src/app/components/modules/regime-table/regime-table.component.ts
@@ -1,18 +1,21 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { Regime } from 'src/app/classes/regime';
 import { EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
     selector: 'app-regime-table',
     templateUrl: './regime-table.component.html',
     styleUrls: ['./regime-table.component.scss'],
-    imports: [NgFor],
+    imports: [NgFor, NgIf, FormsModule, IonButton, IonIcon],
     standalone: true
 })
 export class RegimeTableComponent implements OnInit {
 
     @Input() regimeList: Regime[] = []
+    @Input() isEditable: boolean = false
 
     @Output() onAction = new EventEmitter<Regime>()
 
