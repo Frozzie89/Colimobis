bf58e14f362867fdb073a8906cba268a09733ba3
Frozzie89 <froztor85@gmail.com>
2024-11-01 12:34:18 +0100

    new admin hub page

--- src/app/components/admin/admin-hub/admin-hub.component.ts
+++ src/app/components/admin/admin-hub/admin-hub.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { HeaderComponent } from "../../modules/header/header.component";
 
 @Component({
     selector: 'app-admin-hub',
     templateUrl: './admin-hub.component.html',
     styleUrls: ['./admin-hub.component.scss'],
+    imports: [IonContent, IonGrid, IonRow, IonCol, HeaderComponent],
     standalone: true
 })
 export class AdminHubComponent implements OnInit {
+
+    title = 'Menu admin'
 
     constructor() { }
 
