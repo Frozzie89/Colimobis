034b0215a65b61a1c5f4176a07f422931fa467f0
Frozzie89 <froztor85@gmail.com>
2024-11-07 23:15:27 +0100

    clear interval when buttons are clicked

--- src/app/components/modules/end-form/end-form.component.ts
+++ src/app/components/modules/end-form/end-form.component.ts
@@ -12,21 +12,31 @@ import { IonIcon, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone'
 export class EndFormComponent implements OnInit {
 
     stopwatch = 6
+    intervalId: any
 
     constructor(
         private router: Router
     ) { }
 
     ngOnInit() {
-        let intervalId = setInterval(() => {
+        this.intervalId = setInterval(() => {
             if (this.stopwatch <= 0) {
                 this.router.navigate(['login'])
                 this.stopwatch = 6
-                clearInterval(intervalId)
+                clearInterval(this.intervalId)
             }
 
             this.stopwatch -= 1
         }, 1000);
+    }
+
+    toRegimeSearch() {
+        clearInterval(this.intervalId)
+        this.router.navigate(['/regime-search'])
+    }
+    toLogin() {
+        clearInterval(this.intervalId)
+        this.router.navigate(['/login'])
     }
 
 }
