94e0dfa4e221d46b6d9daa02d1ad49dd3c020cf8
Frozzie89 <froztor85@gmail.com>
2024-11-07 22:55:01 +0100

    routing added

--- src/app/components/modules/end-form/end-form.component.ts
+++ src/app/components/modules/end-form/end-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { IonIcon, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone'
 
 @Component({
     selector: 'module-end-form',
     templateUrl: './end-form.component.html',
     styleUrls: ['./end-form.component.scss'],
-    imports: [IonIcon, IonGrid, IonRow, IonCol],
+    imports: [IonIcon, IonGrid, IonRow, IonCol, RouterModule],
     standalone: true
 })
 export class EndFormComponent implements OnInit {
