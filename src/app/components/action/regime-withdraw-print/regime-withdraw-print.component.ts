67b83571337bfae82d3615087693ca4775d5e390
Frozzie89 <froztor85@gmail.com>
2024-11-07 21:55:57 +0100

    component html/css  is complete

--- src/app/components/action/regime-withdraw-print/regime-withdraw-print.component.ts
+++ src/app/components/action/regime-withdraw-print/regime-withdraw-print.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone'
+import { IonContent, IonGrid, IonRow, IonCol, IonIcon } from '@ionic/angular/standalone'
+import { HeaderComponent } from "../../modules/header/header.component";
 
 
 @Component({
     selector: 'app-regime-withdraw-print',
     templateUrl: './regime-withdraw-print.component.html',
     styleUrls: ['./regime-withdraw-print.component.scss'],
-    imports: [IonContent, IonGrid, IonRow, IonCol],
+    imports: [IonContent, IonGrid, IonRow, IonCol, IonIcon, HeaderComponent],
     standalone: true,
 })
 export class RegimeWithdrawPrintComponent implements OnInit {
+    title = 'Démarrer un chantier'
 
     constructor() { }
 
