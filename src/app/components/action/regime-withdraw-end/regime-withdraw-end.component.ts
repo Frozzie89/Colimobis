37c6d30a12efbc4fcfdabfb6aad17e5e239d8eba
Frozzie89 <froztor85@gmail.com>
2024-11-07 23:10:55 +0100

    set regime state to DEMARRE

--- src/app/components/action/regime-withdraw-end/regime-withdraw-end.component.ts
+++ src/app/components/action/regime-withdraw-end/regime-withdraw-end.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone'
 import { EndFormComponent } from "../../modules/end-form/end-form.component";
+import { RegimeDataService } from 'src/app/service/regime-data.service';
+import { Regime, RegimeState } from 'src/app/classes/regime';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-regime-withdraw-end',
@@ -11,8 +14,21 @@ import { EndFormComponent } from "../../modules/end-form/end-form.component";
 })
 export class RegimeWithdrawEndComponent implements OnInit {
 
-    constructor() { }
+    constructor(
+        private regimeService: RegimeDataService,
+        private route: ActivatedRoute
+    ) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        const id = this.route.snapshot.params['id']
+        this.regimeService.regimeList$.subscribe(regimes => {
+            const regimeJson = regimes.find(regime => regime._id.includes(id))
+            let regime = Regime.fromJson(regimeJson)
+
+            regime.state = RegimeState.DEMARRE
+
+            this.regimeService.update(regime)
+        })
+    }
 
 }
