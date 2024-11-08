0c83ac136a9614437a2506ba3975205a33b1915d
Frozzie89 <froztor85@gmail.com>
2024-11-08 14:03:06 +0100

    equiment and activity are displayed correctly

--- src/app/components/action/regime-restore-end/regime-restore-end.component.ts
+++ src/app/components/action/regime-restore-end/regime-restore-end.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone'
 import { EndFormComponent } from "../../modules/end-form/end-form.component";
+import { ActivatedRoute } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-regime-restore-end',
     templateUrl: './regime-restore-end.component.html',
     styleUrls: ['./regime-restore-end.component.scss'],
-    imports: [IonContent, IonGrid, IonRow, IonCol, EndFormComponent],
+    imports: [IonContent, IonGrid, IonRow, IonCol, EndFormComponent, NgIf],
     standalone: true
 })
 export class RegimeRestoreEndComponent implements OnInit {
 
-    constructor() { }
+    equipment!: boolean
+    activity!: boolean
 
-    ngOnInit() { }
+    constructor(
+        private route: ActivatedRoute
+    ) { }
+
+    ngOnInit() {
+        this.route.queryParams.subscribe(params => {
+            this.equipment = params['equipment'] === 'true'
+            this.activity = params['activity'] === 'true'
+        });
+    }
 
 }
