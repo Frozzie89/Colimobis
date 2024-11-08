80e651447fefdc1373b4800ff25cbf98bed3a59c
Frozzie89 <froztor85@gmail.com>
2024-11-08 19:28:18 +0100

    create and save audits on regime actions

--- src/app/components/action/regime-restore-end/regime-restore-end.component.ts
+++ src/app/components/action/regime-restore-end/regime-restore-end.component.ts
@@ -5,7 +5,10 @@ import { ActivatedRoute } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { RegimeDataService } from 'src/app/service/regime-data.service';
 import { Regime, RegimeState } from 'src/app/classes/regime';
-import { map, switchMap } from 'rxjs/operators';
+import { distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
+import { Audit } from 'src/app/classes/audit';
+import { AuditDataService } from 'src/app/service/audit-data.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
     selector: 'app-regime-restore-end',
@@ -18,10 +21,13 @@ export class RegimeRestoreEndComponent implements OnInit {
 
     equipment!: boolean
     activity!: boolean
+    regimeStateBefore!: RegimeState
+    regimeStateCurrent!: RegimeState
 
     constructor(
         private route: ActivatedRoute,
-        private regimeService: RegimeDataService
+        private regimeService: RegimeDataService,
+        private auditService: AuditDataService
     ) { }
 
 
@@ -32,6 +38,8 @@ export class RegimeRestoreEndComponent implements OnInit {
                 this.activity = params['activity'] === 'true';
 
                 const newRegimeState = this.getState(this.equipment, this.activity);
+                this.regimeStateCurrent = newRegimeState;
+
                 return { newRegimeState, id: this.route.snapshot.params['id'] };
             }),
             switchMap(({ newRegimeState, id }) =>
@@ -39,16 +47,30 @@ export class RegimeRestoreEndComponent implements OnInit {
                     map(regimes => {
                         const regimeJson = regimes.find(regime => regime._id.includes(id));
                         const regime = Regime.fromJson(regimeJson);
+
+                        this.regimeStateBefore = regime.state;
                         regime.state = newRegimeState;
 
                         return regime;
-                    })
+                    }),
+                    distinctUntilChanged((prev, curr) => prev.state === curr.state) // Prevent duplicate emissions for same state
                 )
-            )
-        ).subscribe(regime => {
-            this.regimeService.update(regime);
-        });
+            ),
+            tap(regime => {
+                this.regimeService.update(regime);
+
+                const audit = new Audit(
+                    uuidv4(), // Generate a unique _id for the audit entry
+                    new Date(),
+                    regime._id,
+                    this.regimeStateBefore,
+                    this.regimeStateCurrent
+                );
+                this.auditService.save(audit);
+            })
+        ).subscribe();
     }
+
     private getState(equipment: boolean, activity: boolean): RegimeState {
         const states: RegimeState[][] = [
             [RegimeState.RETURNED_UNDONE, RegimeState.ERROR],
--- src/app/service/audit-data.service.spec.ts
+++ src/app/service/audit-data.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { AuditDataService } from './audit-data.service';
 
 describe('AuditDataService', () => {
-  let service: AuditDataService;
+    let service: AuditDataService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuditDataService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(AuditDataService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
