ed34563822a090132e2fb6d4b4a1f7ab611ffa72
Frozzie89 <froztor85@gmail.com>
2024-11-13 20:26:53 +0100

    defined app ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Colimobis',
-  webDir: 'www'
+    appId: 'com.colimobis.app',
+    appName: 'Colimobis',
+    webDir: 'www'
 };
 
 export default config;
